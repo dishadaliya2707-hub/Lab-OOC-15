a52a2203c90642f5fb508c365b15469644583006
dishadaliya2707-hub <dishadaliya2707@gmail.com>
2026-08-07 05:52:34 +0000

    file

--- 240726/file1.cpp
+++ 240726/file1.cpp
@@ -1,9 +1,12 @@
 #include<iostream>
-namespace syc{
-    int show()
-        {
-            std::cout<<"welcome syc";
-            std::cout<<"perform c++ practical";
-            return 0;
-        }
+namespace syc
+{
+ int show()
+ {
+     std::cout<<"Welcome SYC";
+    std::cout<<"Perform c++ practical";
+    return 0;
+
+ }
+
 }
